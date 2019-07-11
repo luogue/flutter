8ece266bc190a0c5b899e083034e29afdfb59423
yangyue <1175369079@qq.com>
2019-07-11 12:08:34 +0800

    webview是处理不了了

--- lib/pages/wechat/webview.dart
+++ lib/pages/wechat/webview.dart
@@ -5,6 +5,7 @@ import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 // import 'package:webview_flutter/webview_flutter.dart';
 
+
 class WebViewPage extends StatefulWidget {
   WebViewPage({Key key}) : super(key: key);
 
@@ -13,8 +14,6 @@ class WebViewPage extends StatefulWidget {
 }
 
 class _WebViewPageState extends State<WebViewPage> {
-  final flutterWebviewPlugin = new FlutterWebviewPlugin();
-
   Future<String> _getFile() async {
     return await rootBundle.loadString('assets/index.html');
   }
@@ -25,6 +24,16 @@ class _WebViewPageState extends State<WebViewPage> {
       future: _getFile(),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
+          // return WebView(
+          //   initialUrl: new Uri.dataFromString(
+          //     snapshot.data,
+          //     mimeType: 'text/html',
+          //     encoding: Encoding.getByName('utf-8'),
+          //   ).toString(),
+          //   javascriptMode: JavascriptMode.unrestricted,
+          //   debuggingEnabled: true,
+          //   javascriptChannels: 1,
+          // );
           return WebviewScaffold(
             appBar: AppBar(
               title: Text('flutter和web页面通信', style: TextStyle(color: Colors.white)),
@@ -33,13 +42,10 @@ class _WebViewPageState extends State<WebViewPage> {
             url: new Uri.dataFromString(
               snapshot.data,
               mimeType: 'text/html',
-              encoding: Encoding.getByName('utf-8')
+              encoding: Encoding.getByName('utf-8'),
             ).toString(),
             withJavascript: true,
             withLocalStorage: true,
-            withLocalUrl: true,
-            appCacheEnabled: true,
-            allowFileURLs: true,
             initialChild: Container(
               color: Colors.deepOrangeAccent,
               child: const Center(
